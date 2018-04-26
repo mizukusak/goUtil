7ad8e7d03c066ec3ef1a419bc8a1d459e5b1415a
fumiaki_shimazu <fumiaki.shimazu@gmail.com>
2018-04-26 10:53:29 +0900

    キャンセル可能な外部コマンド実行関数追加

---
+++ os/exec/exec.go
@@ -0,0 +1,32 @@
+package execUtil
+
+import (
+	"os/exec"
+	"io/ioutil"
+	"context"
+)
+
+func Execute(cmd *exec.Cmd, ctx context.Context) []byte {
+	var out []byte
+	stdout, _ := cmd.StdoutPipe()
+	cmdDone := make(chan bool)
+	cmdStart := make(chan bool)
+
+	go func() {
+		cmd.Start()
+		cmdStart <- true
+		out, _ = ioutil.ReadAll(stdout)
+		cmdDone <- true
+	}()
+
+	<-cmdStart
+	select {
+	case <- cmdDone:
+		go func() {<-ctx.Done()}()
+	case <- ctx.Done():
+		cmd.Process.Kill()
+		<-cmdDone
+	}
+	return out
+}
+
